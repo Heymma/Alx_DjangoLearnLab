aea4e89307b100bd58ab97c7cf08380d24c1021c
Ezeatu Emmanuel <heymma01@gmail.com>
2025-12-13 20:02:05 +0100

    Set default role for UserProfile and finalize role-based views

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -46,7 +46,7 @@ ROLE_CHOICES = (
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="Member")
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
