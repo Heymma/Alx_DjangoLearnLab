459782c8d904b3f728387cdddc3c775fdd26a6a1
Ezeatu Emmanuel <heymma01@gmail.com>
2025-12-22 04:14:18 +0100

    update

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,8 +1,5 @@
 from django import forms
-from .models import Book
 
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title']
+class ExampleForm(forms.Form):
+    example_field = forms.CharField(max_length=100)
