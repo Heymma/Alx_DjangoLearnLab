02469b6edcd161a38f7cafa7b4bd4f3b4bc75d99
Ezeatu Emmanuel <heymma01@gmail.com>
2025-12-22 04:12:07 +0100

    update

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -109,3 +109,13 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "relationship_app.CustomUser"
 AUTH_USER_MODEL = "bookshelf.CustomUser"
+DEBUG = False
+
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+MIDDLEWARE = [
+    'LibraryProject.middleware.CSPMiddleware',
+]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import BookForm
 
 
 @permission_required("bookshelf.can_view", raise_exception=True)
 def book_list(request):
-    books = Book.objects.all()
+    books = Book.objects.all()  # Secure ORM query (no SQL injection risk)
     return render(request, "bookshelf/book_list.html", {"books": books})
+
+
+def secure_form_view(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()  # Secure save with validation (sanitizes input)
+            return render(request, "bookshelf/form_example.html", {"form": form, "message": "Saved securely"})
+    else:
+        form = BookForm()
+    return render(request, "bookshelf/form_example.html", {"form": form})
